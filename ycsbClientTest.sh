97fa6f68acc0c28785a6b4fcad863528a6a1b6fa
João Soares <joao.soares.di@gmail.com>
2019-04-10 17:07:41 +0100

    ycsbClientTest updated

--- ycsbClientTest.sh
+++ ycsbClientTest.sh
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-java -Dlogback.configurationFile="./config/logback.xml" -cp .:./bin:./lib/* bftsmart.demo.ycsb.YCSBClient
+java -Dlogback.configurationFile="./config/logback.xml" -cp .:./bin:./lib/* bftsmart.demo.ycsb.YCSBClient $1 $2
