1c8e403915af2b4580a92bb4bc99efd158a1ea3c
João Soares <joao.soares.di@gmail.com>
2019-04-25 17:12:00 +0100

    x

--- src/bftsmart/tom/server/durability/DurableStateLog.java
+++ src/bftsmart/tom/server/durability/DurableStateLog.java
@@ -282,7 +282,19 @@ public class DurableStateLog extends StateLog {
     			System.arraycopy(ckpState, commonShards[1]*shardSize, data, shardSize, (comm_count-1)*shardSize);
 
         		for(int i = 0;i < noncommonShards.length; i++) {
-    				System.arraycopy(ckpState, noncommonShards[i]*shardSize, data, (comm_count+i)*shardSize, shardSize);
+        			try {
+        				System.arraycopy(ckpState, noncommonShards[i]*shardSize, data, (comm_count+i)*shardSize, shardSize);
+        			} catch (Exception e) {
+        				e.printStackTrace();
+        				
+        				System.out.println(i);
+        				System.out.println(ckpState.length);
+        				System.out.println(noncommonShards[i]);
+        				System.out.println(noncommonShards[i]*shardSize);
+        				System.out.println(data.length);
+        				System.out.println((comm_count+i));
+        				System.out.println((comm_count+i)*shardSize);
+        			}
         		}
         	}
         	else {
